d0b002bb0a5c727eb69e762410752ee05ea92d5d
gabivoicu <gabrielavoicu16@gmail.com>
2017-01-16 19:22:17 -0600

    refactor data file read into separate functions

--- learning_path_generator.py
+++ learning_path_generator.py
@@ -1,30 +1,38 @@
 import csv, sys
 
 
-def main():
-
-    domain_order_file = sys.argv[1]
-    student_test_scores_file = sys.argv[2]
-
-    domains_ordered_by_class = ()
+def get_domains_ordered_by_grade(domain_order_file):
+    domains_ordered_by_grade = ()
     with open(domain_order_file, 'r') as data_file:
         for row in data_file:
             row = row.strip()
             row_elements = row.split(',')
             class_name = row_elements.pop(0)
             row_units = ((class_name, tuple(row_elements)), )
-            domains_ordered_by_class = domains_ordered_by_class + row_units
-            
+            domains_ordered_by_grade = domains_ordered_by_grade + row_units
+    return domains_ordered_by_grade
+
+def get_student_test_scores(student_test_scores_file):
     student_test_scores = []
     with open(student_test_scores_file, 'r') as data_file:
         file_reader = csv.DictReader(data_file)
         for row in file_reader:
             student_test_scores.append(row)
+    return student_test_scores
+
+def main():
+
+    domain_order_file = sys.argv[1]
+    student_test_scores_file = sys.argv[2]
+
+    domains_ordered_by_grade = get_domains_ordered_by_grade(domain_order_file)    
+    student_test_scores = get_student_test_scores(student_test_scores_file)
 
     for student_test_score in student_test_scores:
         student_units = ()
-        for grade in range(0, len(domains_ordered_by_class)):
-            units_for_grade = domains_ordered_by_class[grade][1]
+        for grade_with_units in domains_ordered_by_grade:
+            grade = grade_with_units[0]
+            units_for_grade = grade_with_units[1]
             for unit in units_for_grade:
                 if student_test_score[unit] == 'K':
                     student_test_score[unit] = 0
@@ -35,6 +43,7 @@ def main():
                 else:
                     pass
         print student_units[:5]
+        print student_test_score['Student Name']
 
 if __name__ == '__main__':
     main()
