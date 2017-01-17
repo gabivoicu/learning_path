6e49cb0518639f86abfc58503fb2664938e2dac8
gabivoicu <gabrielavoicu16@gmail.com>
2017-01-16 19:30:35 -0600

    refactor creation of student paths into function rename existing functions for clarity add commented out function to write results to file

--- learning_path_generator.py
+++ learning_path_generator.py
@@ -1,7 +1,7 @@
 import csv, sys
 
 
-def get_domains_ordered_by_grade(domain_order_file):
+def read_domains_ordered_by_grade_from_file(domain_order_file):
     domains_ordered_by_grade = ()
     with open(domain_order_file, 'r') as data_file:
         for row in data_file:
@@ -12,7 +12,7 @@ def get_domains_ordered_by_grade(domain_order_file):
             domains_ordered_by_grade = domains_ordered_by_grade + row_units
     return domains_ordered_by_grade
 
-def get_student_test_scores(student_test_scores_file):
+def read_student_test_scores_from_file(student_test_scores_file):
     student_test_scores = []
     with open(student_test_scores_file, 'r') as data_file:
         file_reader = csv.DictReader(data_file)
@@ -20,14 +20,8 @@ def get_student_test_scores(student_test_scores_file):
             student_test_scores.append(row)
     return student_test_scores
 
-def main():
-
-    domain_order_file = sys.argv[1]
-    student_test_scores_file = sys.argv[2]
-
-    domains_ordered_by_grade = get_domains_ordered_by_grade(domain_order_file)    
-    student_test_scores = get_student_test_scores(student_test_scores_file)
-
+def create_student_learning_paths(domains_ordered_by_grade, student_test_scores):
+    student_learning_paths = []
     for student_test_score in student_test_scores:
         student_units = ()
         for grade_with_units in domains_ordered_by_grade:
@@ -44,6 +38,18 @@ def main():
                     pass
         print student_units[:5]
         print student_test_score['Student Name']
+        student_learning_paths.append(student_units)
+    return student_learning_paths
+
+def main():
+
+    domain_order_file = sys.argv[1]
+    student_test_scores_file = sys.argv[2]
+
+    domains_ordered_by_grade = read_domains_ordered_by_grade_from_file(domain_order_file)    
+    student_test_scores = read_student_test_scores_from_file(student_test_scores_file)
+    student_learning_paths = create_student_learning_paths(domains_ordered_by_grade, student_test_scores)
+    # write_student_learning_paths_to_file(student_learning_paths)
 
 if __name__ == '__main__':
     main()
